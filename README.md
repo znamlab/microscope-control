2b1200fe0ca9c29d357b061776cdb6ef3452ba36
Petr Znamenskiy <peter@gmail.com>
2020-11-10 18:00:57 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Microscope control
+Useful utilities for microscope control.
+
+### Contents
+* `flipflop.c` - Arduino code for interleaving 405 and 470 nm illumination for haemodynamic correction.
---
+++ src/flipflop/flipflop.c
@@ -0,0 +1,36 @@
+/*
+LED triggering for interleaved 405 / 470 nm illumination
+
+Uses the exposure start trigger (or flash trigger, for rolling shutter cameras)
+to alternate between turning on one or the other LED.
+*/
+const int CameraIn = 2;
+const int LED1 = 3;
+const int LED2 = 4;
+int odd = 1;
+
+void setup() {
+  pinMode(CameraIn, INPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+}
+
+void loop() {
+  while(digitalRead(CameraIn) == LOW) { }
+  
+  if(odd) {
+    digitalWrite(LED1, HIGH);
+    odd = 0;
+    
+    while(digitalRead(CameraIn) == HIGH) { }
+    
+    digitalWrite(LED1, LOW);
+  } else {
+    digitalWrite(LED2, HIGH);
+    odd = 1;
+    
+    while(digitalRead(CameraIn) == HIGH) { }
+    
+    digitalWrite(LED2, LOW);
+  }
+}
